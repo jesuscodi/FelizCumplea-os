8b0578b37e7f30b70c5ef17d671f92c6747497de
Jesus <jesusgp72021@gmail.com>
2025-08-10 07:58:16 -0500

    Update login.js

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 function checkDate() {
     const input = document.getElementById('dob').value.trim();
-    const correctDate = "07/07/2021"; // Cambia por la fecha real de Fátima
+    const correctDate = "11/08/2001"; // Cambia por la fecha real de Fátima
 
     if (input === correctDate) {
         window.location.href = "main.html";
